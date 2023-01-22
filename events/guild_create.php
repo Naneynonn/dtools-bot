6de45a2bce0f8b7b3837ac0a96fcd43f65ed3486
Naneynonn <oddy239@gmail.com>
2023-01-22 15:29:58 +0200

    temp var_dump locale

--- events/guild_create.php
+++ events/guild_create.php
@@ -7,8 +7,12 @@ use Discord\Parts\Guild\Guild;
 $discord->on(Event::GUILD_CREATE, function (Guild $guild, Discord $discord) {
   $locale = ['en', 'uk', 'en'];
 
+  var_dump($guild->preferred_locale);
+
   $guild->preferred_locale = substr($guild->preferred_locale, 0, 2);
   $guild->preferred_locale = in_array($guild->preferred_locale, $locale) ? $guild->preferred_locale : $locale[0];
+
+  var_dump($guild->preferred_locale);
 
   $model = new DB();
   $model->createGuildInfo(name: $guild->name, lang: $guild->preferred_locale, icon: $guild->icon_hash, members_online: 0, members_all: $guild->member_count, server_id: $guild->id);
