df3590b3c7cc302c78dde4cc940bd34a92fdd875
Naneynonn <oddy239@gmail.com>
2023-04-13 16:12:54 +0300

    fix close pdo

--- components/message_prev.php
+++ components/message_prev.php
@@ -8,21 +8,23 @@ $stop = false;
 if ($message->author?->bot || !$message->content) return;
 
 $model = new Model();
-$settings = $model->getSettingsServer(id: $message->guild->id);
-if (!$settings || !$settings['is_enable']) return;
 
-$perm = $model->getServerPerm(id: $message->guild->id, module: 'automod');
-if (getIgnoredPermissions(perm: $perm, message: $message, selection: 'all')) return;
+try {
+  $settings = $model->getSettingsServer(id: $message->guild->id);
+  if (!$settings || !$settings['is_enable']) return;
 
-// Load Lang
-$lng = new Language(lang: $settings['lang']);
+  $perm = $model->getServerPerm(id: $message->guild->id, module: 'automod');
+  if (getIgnoredPermissions(perm: $perm, message: $message, selection: 'all')) return;
 
-require 'components/caps.php';
-require 'components/replace.php';
-require 'components/message_badwords.php';
+  // Load Lang
+  $lng = new Language(lang: $settings['lang']);
 
-$settings = null;
-$perm = null;
+  require 'components/caps.php';
+  require 'components/replace.php';
+  require 'components/message_badwords.php';
 
-$lng->close();
-$model->close();
+  $settings = null;
+  $perm = null;
+} finally {
+  $model->close();
+}
