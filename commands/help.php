56a0c1d5324c7272f3fc295a416e73b46a4cea7f
Naneynonn <oddy239@gmail.com>
2023-05-01 14:27:45 +0300

    update help

--- commands/help.php
+++ commands/help.php
@@ -18,7 +18,7 @@ $discord->listenCommand('help', function (Interaction $interaction) use ($discor
     ->addField(['name' => $lng->get('embeds.commands'), 'value' => $lng->get('commands.main'), 'inline' => 'false'])
     ->addField(['name' => $lng->get('embeds.help.settings'), 'value' => $lng->get('commands.automod'), 'inline' => 'false'])
     ->addField(['name' => $lng->get('embeds.filters'), 'value' => $lng->get('commands.filters'), 'inline' => 'false'])
-    // ->addField(['name' => $lng->get('embeds.info'), 'value' => $lng->get('embeds.help.info'), 'inline' => 'false'])
+    ->addField(['name' => $lng->get('embeds.info'), 'value' => $lng->get('embeds.help.info'), 'inline' => 'false'])
     ->setThumbnail('https://cdn.discordapp.com/attachments/525360788924399637/1099658819023421470/favicon.png');
 
   $interaction->respondWithMessage(builder: MessageBuilder::new()->addEmbed($embed), ephemeral: true);
