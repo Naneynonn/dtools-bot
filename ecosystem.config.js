0b1fe9de84571ab4cf05d9cc0aafcaa0c04be546
Naneynonn <oddy239@gmail.com>
2023-12-07 21:48:22 +0200

    update start file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,19 @@
+const shardCount = 2;
+
 module.exports = {
-  apps: [
-    {
-      name: "dtools0-fnrr",
+  apps: Array.from({ length: shardCount }, (_, i) => {
+    const appName = `dtools${i}-fnrr`;
+
+    return {
+      name: appName,
       script: "index.php",
       interpreter: "php",
       max_memory_restart: "200M",
-      args: "0 2",
+      args: `${i} ${shardCount}`,
       watch: false,
       log_date_format: "YYYY-MM-DD HH:mm:ss",
-      out_file: "./logs/dtools0-out.log",
-      error_file: "./logs/dtools0-error.log",
+      out_file: `./logs/${appName}-out.log`,
+      error_file: `./logs/${appName}-error.log`,
       min_uptime: 1000,
       restart_delay: 2000,
       max_restarts: 10,
@@ -19,26 +23,6 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
       },
-    },
-    {
-      name: "dtools1-fnrr",
-      script: "index.php",
-      interpreter: "php",
-      max_memory_restart: "200M",
-      args: "1 2",
-      watch: false,
-      log_date_format: "YYYY-MM-DD HH:mm:ss",
-      out_file: "./logs/dtools1-out.log",
-      error_file: "./logs/dtools1-error.log",
-      min_uptime: 1000,
-      restart_delay: 2000,
-      max_restarts: 10,
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      },
-    },
-  ],
-}
+    };
+  }),
+};
